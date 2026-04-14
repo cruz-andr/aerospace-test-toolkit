ace25229c1c53cd968141e9cbc8c5b8dbd31e499
cruz-andr <cruz.andr@northeastern.edu>
2026-04-14 17:49:53 -0400

    cleaned the format of the check_limits outputs for engineers to easily read

--- analyzer.py
+++ analyzer.py
@@ -13,15 +13,18 @@ limits = {"pressure_psi": [95, 105],
           "current_a" : [4.5, 5.5]}
 
 
-
+#example of print => [ANOMALY] timestamp 15 | pressure_psi = 130 | exceeds max of 105
 def check_limits():
     for i in limits:
         dataPastMax = data[data[i] > limits[i][1]]
         dataLowerMin = data[data[i] < limits[i][0]]
-        if not dataPastMax.empty:
-            print(dataPastMax)
-        if not dataLowerMin.empty:
-            print(dataLowerMin)
+
+        for index, row in dataPastMax.iterrows():
+            print(f"[ANOMALY] timestamp {row['timestamp']} | {i} = {row[i]} | exceeds max of {limits[i][1]}")
+        for index, row in dataLowerMin.iterrows():
+            print(f"[ANOMALY] timestamp {row['timestamp']} | {i} = {row[i]} | exceeds min of {limits[i][0]}")
+        
+
 
 def main():
     check_limits()
